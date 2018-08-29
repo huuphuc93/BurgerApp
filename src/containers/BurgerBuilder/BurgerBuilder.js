ad791eec9366f95f0e4f5c7f2148cda3e66155b6
mai.huu.phuc <FRAMGIA\mai.huu.phuc@framgia0377-lt.framgia.com>
2018-08-29 16:58:15 +0700

    build control component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,72 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+};
 class BurgerBuilder extends Component {
+  // constructor(props) {
+    // super(props);
+  // }
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updateCount = oldCount + 1;
+    const updateIngredient = {
+      ...this.state.ingredients
+    };
+    updateIngredient[type] = updateCount
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updateIngredient});
+  }
+
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if(oldCount <= 0) {
+      return;
+    }
+    const updateCount = oldCount - 1;
+    const updateIngredient = {
+      ...this.state.ingredients
+    };
+    updateIngredient[type] = updateCount
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({totalPrice: newPrice, ingredients: updateIngredient});
+  }
+
   render () {
+    const disableInfo = {
+      ...this.state.ingredients
+    };
+    for(let key in disableInfo) {
+      disableInfo[key] = disableInfo[key] <= 0
+    }
     return (
       <Aux>
-        <div>Burger</div>
-        <div>Burger Controls</div>
+        <Burger ingredients={this.state.ingredients}/>
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemove={this.removeIngredientHandler}
+          disable={disableInfo} />
       </Aux>
     );
   }
