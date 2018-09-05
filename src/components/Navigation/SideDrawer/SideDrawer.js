c8a9e193bed2583a24a6854a5e21a944e18b1f23
mai.huu.phuc <FRAMGIA\mai.huu.phuc@framgia0377-lt.framgia.com>
2018-09-05 15:46:53 +0700

    finish app

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,26 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import BackDrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux/Aux';
 
 const sideDrawer = (props) => {
+  let attachedClass = [classes.SideDrawer, classes.Close]
+  if (props.open) {
+    attachedClass = [classes.SideDrawer, classes.Open]
+  }
   return (
-    <div className={classes.SideDrawer} height="11px">
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <BackDrop show={props.open} clicked={props.closed}/>
+      <div className={attachedClass.join(' ')} height="11px">
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
